f515bbe65e04d8355dc4fc0a9fc46497e642df7c
Omar Abdelwahab <lebda74@yahoo.com>
2021-04-20 06:43:33 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <Route path='/' component={HomeScreen} exact/>
+          <Route path="/products/:id" component={ProductScreen} />
+        </Container>
+      </main>
+
+      <Footer/>
+    </Router>
   );
 }
 
